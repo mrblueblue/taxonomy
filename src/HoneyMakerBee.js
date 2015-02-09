a0b7b28d7bc5ffe33b9a617e3e373272ed19b58e
Jonathan Huang <grokut@gmail.com>
2015-02-09 12:18:45 -0800

    finished Bee making

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+
+	Grub.call(this);
+
+	this.age = 10;
+	this.job = 'make honey';
+	this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Grub.prototype)
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot+=1
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot-=1
+}
